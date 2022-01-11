f3f6de1f63946b066ca824fef225cdce51e1c3fe
LooWanYing <looying20@gmail.com>
2022-01-11 17:11:55 +0800

    Update Product.py

--- Product.py
+++ Product.py
@@ -1,9 +1,37 @@
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
+import User            # import 'module'
+from User import User       # from 'module' import 'class'
 
-@app.route('/index', methods=['GET', 'POST'])
-def lionel():
-    return render_template('index.html')
+class Customer(User):
+    count_id = 0
 
-if __name__ == '__main__':
-    app.run()
+    def _init_(self, product_name, last_name, gender, membership, remarks, email, date_joined, address):
+       super().init(product_name, last_name, gender, membership, remarks)
+       Customer.count_id += 1
+       self.__customer_id = Customer.count_id
+       self.__email = email
+       self.__date_joined = date_joined
+       self.__address = address
+
+    def get_customer_id(self):
+        return self.__customer_id
+
+    def get_email(self):
+        return self.__email
+
+    def get_date_joined(self):
+        return self.__date_joined
+
+    def get_address(self):
+        return self.__address
+
+    def set_customer_id(self, customer_id):
+        self.__customer_id = customer_id
+
+    def set_email(self, email):
+        self.__email = email
+
+    def set_date_joined(self, date_joined):
+        self.__date_joined = date_joined
+
+    def set_address(self, address):
+        self.__address = address
